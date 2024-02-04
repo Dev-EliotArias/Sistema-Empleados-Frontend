fffa4afddbf395669fed181180f989b009ae560f
Dev-EliotArias <develiotarias@gmail.com>
2024-02-04 00:18:35 -0500

    feat: edit employees, delete employees, validate data

--- src/app/lista-empleados/lista-empleados.component.ts
+++ src/app/lista-empleados/lista-empleados.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Empleado } from '../empleado';
 import { EmpleadoService } from '../empleado.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-lista-empleados',
@@ -9,17 +11,57 @@ import { EmpleadoService } from '../empleado.service';
 })
 export class ListaEmpleadosComponent implements OnInit {
 
+  borrar = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-success",
+    cancelButton: "btn btn-danger mr-2"
+  },
+  buttonsStyling: false
+});
+
   empleados:Empleado[];
-  constructor(private empleadoServicio:EmpleadoService) { }
+  constructor(private empleadoServicio:EmpleadoService, private router:Router) { }
 
   ngOnInit(): void {
     this.obtenerEmpleados();
   }
+
+  actualizarEmpleado(id:number){
+    this.router.navigate(['actualizar-empleado', id]);
+  }
+
+
+
+  eliminarEmpleado(id:number){
+      this.borrar.fire({
+        title: '¿Estas seguro?',
+        text: "Confirma si deseas eliminar al empleado",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        confirmButtonText: 'Si , elimínalo',
+        cancelButtonText: 'No, cancelar',
+        reverseButtons: true
+      }).then((result) => {
+        if(result.value){
+          this.empleadoServicio.eliminarEmpleado(id).subscribe(dato => {
+            console.log(dato);
+            this.obtenerEmpleados();
+            Swal.fire(
+              'Empleado eliminado',
+              'El empleado ha sido eliminado con exito',
+              'success'
+            )
+          })
+        }
+      })
+    }
 
   private obtenerEmpleados(){
     this.empleadoServicio.obtenerListaDeEmpleados().subscribe(dato => {
       this.empleados = dato
     });
   }
+
 
 }
